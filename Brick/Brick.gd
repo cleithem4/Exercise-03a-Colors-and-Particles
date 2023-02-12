fce5540adcf940597e59059e808bd6a56cb6c51f
cleithem4 <c4leithem@gmail.com>
2023-02-12 15:43:49 -0500

    assignment done

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -6,9 +6,27 @@ var dying = false
 
 func _ready():
 	position = new_position
+	if score >= 100:
+		$ColorRect.color = Color8(224,49,49)
+	elif score >= 90:
+		$ColorRect.color = Color8(253,126,20)
+	elif score >= 80:
+		$ColorRect.color = Color8(218,119,242)
+	elif score >= 70:
+		$ColorRect.color = Color8(153,233,242)
+	elif score >= 60:
+		$ColorRect.color = Color8(105,219,124)
+	elif score >= 50:
+		$ColorRect.color = Color8(255,212,59)
+	elif score >= 40:
+		$ColorRect.color = Color8(255,146,43)
+	elif score >= 30:
+		$ColorRect.color = Color8(102,168,15)
+	elif score >= 20:
+		$ColorRect.color = Color8(253,126,20)
 
 func _physics_process(_delta):
-	if dying:
+	if dying and not $Confetti.emitting:
 		queue_free()
 
 func hit():
@@ -20,3 +38,4 @@ func die():
 	$ColorRect.hide()
 	Global.update_score(score)
 	get_parent().check_level()
+	$Confetti.emitting = true
